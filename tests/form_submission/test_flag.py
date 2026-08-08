ede1d47df130b81b9cb29345348880c557fe06b9
Joshua Brooks <josh.vdbroek@gmail.com>
2026-08-08 22:15:31 +0700

    feat: flag assignment and triage workflow in admin (refs #35)
    - Add Flag.assigned_to / assigned_at fields and is_resolved property
    - Surface unresolved-flag status as a 'Flagged' column on Submission and
      SeparatedSubmission admins via the with_unresolved_flags() annotation
    - Add FlagInline to SeparatedSubmissionAdmin
    - FlagAdmin: resolution/assignment filters, assign-to-me and mark-resolved
      bulk actions, auto-fill of created_by/resolved_by/assigned_at

--- tests/form_submission/test_flag.py
+++ tests/form_submission/test_flag.py
@@ -1,5 +1,9 @@
 import pytest
+from django.contrib.admin.sites import AdminSite
+from django.contrib.auth import get_user_model
+from django.test import RequestFactory
 
+from formkit_ninja.admin import FlagAdmin
 from formkit_ninja.form_submission.models import Flag, SeparatedSubmission
 
 
@@ -57,3 +61,66 @@ class TestFlag:
         flags = list(Flag.objects.filter(separated_submission=separated_submission))
         assert flags[0].pk == f2.pk  # newest first
         assert flags[1].pk == f1.pk  # oldest second
+
+    def test_assignment(self, separated_submission: SeparatedSubmission) -> None:
+        """A flag can be assigned to a user, queryable via user.assigned_flags."""
+        user = get_user_model().objects.create(username="triager")
+        flag = Flag.objects.create(
+            separated_submission=separated_submission,
+            flag_type="needs_review",
+            message="Please review",
+            assigned_to=user,
+        )
+        assert flag.is_resolved is False
+        assert list(user.assigned_flags.all()) == [flag]
+
+
+@pytest.mark.django_db
+class TestFlagAdmin:
+    def _admin(self) -> FlagAdmin:
+        return FlagAdmin(Flag, AdminSite())
+
+    def test_save_model_sets_created_by_on_add(self, separated_submission: SeparatedSubmission) -> None:
+        """Adding a flag in the admin defaults created_by to the request user."""
+        user = get_user_model().objects.create(username="creator")
+        request = RequestFactory().post("/")
+        request.user = user
+        flag = Flag(separated_submission=separated_submission, flag_type="r", message="m")
+        self._admin().save_model(request, flag, form=None, change=False)
+        assert flag.created_by == user
+
+    def test_save_model_sets_resolved_by(self, separated_submission: SeparatedSubmission) -> None:
+        """Setting resolved_at in the admin auto-fills resolved_by."""
+        from django.utils import timezone
+
+        user = get_user_model().objects.create(username="resolver")
+        request = RequestFactory().post("/")
+        request.user = user
+        flag = Flag.objects.create(separated_submission=separated_submission, flag_type="r", message="m")
+        flag.resolved_at = timezone.now()
+        self._admin().save_model(request, flag, form=None, change=True)
+        assert flag.resolved_by == user
+
+    def test_assign_to_me_action(self, separated_submission: SeparatedSubmission) -> None:
+        """The assign_to_me action assigns selected flags to the request user."""
+        user = get_user_model().objects.create(username="me")
+        request = RequestFactory().post("/")
+        request.user = user
+        request._messages = type("M", (), {"add": lambda *a, **k: None})()
+        flag = Flag.objects.create(separated_submission=separated_submission, flag_type="r", message="m")
+        self._admin().assign_to_me(request, Flag.objects.filter(pk=flag.pk))
+        flag.refresh_from_db()
+        assert flag.assigned_to == user
+        assert flag.assigned_at is not None
+
+    def test_mark_resolved_action(self, separated_submission: SeparatedSubmission) -> None:
+        """The mark_resolved action resolves unresolved selected flags."""
+        user = get_user_model().objects.create(username="closer")
+        request = RequestFactory().post("/")
+        request.user = user
+        request._messages = type("M", (), {"add": lambda *a, **k: None})()
+        flag = Flag.objects.create(separated_submission=separated_submission, flag_type="r", message="m")
+        self._admin().mark_resolved(request, Flag.objects.filter(pk=flag.pk))
+        flag.refresh_from_db()
+        assert flag.resolved_at is not None
+        assert flag.resolved_by == user
